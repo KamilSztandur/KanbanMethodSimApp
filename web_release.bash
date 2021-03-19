7ceb7cf1c8c33aa03cd43b8580ecbb1841cde818
KamilSztandur <kamil.sztandur@vp.pl>
2021-03-19 15:09:35 +0100

    Add web-release for github pages builder
    - added bash script which builds web-release for github pages.
    - added first web release build.

--- web_release.bash
+++ web_release.bash
@@ -3,9 +3,32 @@
 # Build and run web app
 echo 'Starting...'
 
+
+echo 'Building web release...'
+
 cd kanbansim
 flutter pub get
 flutter build web
-flutter run -d chrome
-
+cd ..
 echo 'Finished.'
+
+
+echo 'Building github webpages directory...'
+rm -r docs
+mkdir docs
+cp -r kanbansim/build/web/* docs/.
+
+# Rename paths for github weg pages
+cd docs
+sed -i 's/"assets\/"/"KanbanMethodSimApp\/assets\/"/g' main.dart.js
+sed -i "s/'main.dart.js';/'KanbanMethodSimApp\/main.dart.js';/g" index.html
+cd ..
+echo 'Finished.'
+
+
+#echo 'Running web app on localhost...'
+#flutter run chrome
+#echo 'Finished.'
+
+
+echo 'Script finished.'
