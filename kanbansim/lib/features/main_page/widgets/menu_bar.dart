3836e281aceef02eee6f5bb58f1d255c7b9d582d
KamilSztandur <kamil.sztandur@vp.pl>
2021-04-08 08:49:33 +0200

    Restructure MenuBar subwidgets generation.
    - separate _buildWidget() functions into different private classes.

--- kanbansim/lib/features/main_page/widgets/menu_bar.dart
+++ kanbansim/lib/features/main_page/widgets/menu_bar.dart
@@ -14,10 +14,52 @@ class MainMenuBar extends StatelessWidget {
   final Function(String) loadSimStateFromFilePath;
   final VoidCallback saveSimStateIntoFile;
 
+  MainMenuBar({
+    Key key,
+    @required this.addRandomTasks,
+    @required this.clearAllTasks,
+    @required this.loadSimStateFromFilePath,
+    @required this.saveSimStateIntoFile,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: KanbanSimApp.of(context).isWeb() ? 45 : 76,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.blueAccent.withOpacity(0.8), Colors.blueAccent],
+          begin: Alignment.topLeft,
+          end: Alignment.topRight,
+          stops: [0.0, 0.8],
+          tileMode: TileMode.clamp,
+        ),
+      ),
+      child: Column(
+        children: [
+          KanbanSimApp.of(context).isWeb() ? Container() : WindowBar(),
+          _ToolBar(
+            addRandomTasks: this.addRandomTasks,
+            clearAllTasks: this.clearAllTasks,
+            saveSimStateIntoFile: this.saveSimStateIntoFile,
+            loadSimStateFromFilePath: this.loadSimStateFromFilePath,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ToolBar extends StatelessWidget {
+  final VoidCallback addRandomTasks;
+  final VoidCallback clearAllTasks;
+  final Function(String) loadSimStateFromFilePath;
+  final VoidCallback saveSimStateIntoFile;
+
   LoadFilePopup pickFilePopup;
   SaveFilePopup saveFilePopup;
 
-  MainMenuBar({
+  _ToolBar({
     Key key,
     @required this.addRandomTasks,
     @required this.clearAllTasks,
@@ -42,174 +84,155 @@ class MainMenuBar extends StatelessWidget {
     _initializeFilePickerPopup();
     _initializeFileSaverPopup();
 
-    return Container(
-      height: KanbanSimApp.of(context).isWeb() ? 45 : 76,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Colors.blueAccent.withOpacity(0.8), Colors.blueAccent],
-          begin: Alignment.topLeft,
-          end: Alignment.topRight,
-          stops: [0.0, 0.8],
-          tileMode: TileMode.clamp,
+    return PlutoMenuBar(
+      backgroundColor: Colors.blueAccent.withOpacity(0.8),
+      menuIconColor: Colors.white,
+      textStyle: TextStyle(color: Colors.white),
+      menus: [
+        MenuItem(
+          title: AppLocalizations.of(context).newSession,
+          icon: Icons.create_outlined,
+          children: [
+            MenuItem(
+              title: AppLocalizations.of(context).createEmptySession,
+              icon: Icons.create_new_folder_outlined,
+              onTap: () {
+                SubtleMessage.messageWithContext(
+                  context,
+                  AppLocalizations.of(context).createEmptySession,
+                );
+              },
+            ),
+            MenuItem(
+              title: AppLocalizations.of(context).selectTemplate,
+              icon: Icons.file_copy_outlined,
+              onTap: () {
+                SubtleMessage.messageWithContext(
+                  context,
+                  AppLocalizations.of(context).selectingTemplateNotif,
+                );
+              },
+            ),
+          ],
         ),
-      ),
-      child: Column(
-        children: [
-          KanbanSimApp.of(context).isWeb() ? Container() : WindowBar(),
-          PlutoMenuBar(
-            backgroundColor: Colors.blueAccent.withOpacity(0.8),
-            menuIconColor: Colors.white,
-            textStyle: TextStyle(color: Colors.white),
-            menus: [
-              MenuItem(
-                title: AppLocalizations.of(context).newSession,
-                icon: Icons.create_outlined,
-                children: [
-                  MenuItem(
-                    title: AppLocalizations.of(context).createEmptySession,
-                    icon: Icons.create_new_folder_outlined,
-                    onTap: () {
-                      SubtleMessage.messageWithContext(
-                        context,
-                        AppLocalizations.of(context).createEmptySession,
-                      );
-                    },
-                  ),
-                  MenuItem(
-                    title: AppLocalizations.of(context).selectTemplate,
-                    icon: Icons.file_copy_outlined,
-                    onTap: () {
-                      SubtleMessage.messageWithContext(
-                        context,
-                        AppLocalizations.of(context).selectingTemplateNotif,
-                      );
-                    },
-                  ),
-                ],
-              ),
-              MenuItem(
-                title: AppLocalizations.of(context).saveSession,
-                icon: Icons.save_outlined,
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) =>
-                        this.saveFilePopup.show(context),
-                  );
-                },
-              ),
-              MenuItem(
-                title: AppLocalizations.of(context).loadSession,
-                icon: Icons.read_more_outlined,
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) =>
-                        this.pickFilePopup.show(context),
-                  );
-                },
-              ),
-              MenuItem(
-                title: AppLocalizations.of(context).addRandomTasks,
-                icon: Icons.library_add_outlined,
-                onTap: () {
-                  this.addRandomTasks();
+        MenuItem(
+          title: AppLocalizations.of(context).saveSession,
+          icon: Icons.save_outlined,
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) =>
+                  this.saveFilePopup.show(context),
+            );
+          },
+        ),
+        MenuItem(
+          title: AppLocalizations.of(context).loadSession,
+          icon: Icons.read_more_outlined,
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) =>
+                  this.pickFilePopup.show(context),
+            );
+          },
+        ),
+        MenuItem(
+          title: AppLocalizations.of(context).addRandomTasks,
+          icon: Icons.library_add_outlined,
+          onTap: () {
+            this.addRandomTasks();
 
-                  SubtleMessage.messageWithContext(
-                    context,
-                    AppLocalizations.of(context).addRandomTasksSuccess,
-                  );
-                },
-              ),
-              MenuItem(
-                title: AppLocalizations.of(context).resetSession,
-                icon: Icons.delete_forever_outlined,
-                onTap: () {
-                  this.clearAllTasks();
+            SubtleMessage.messageWithContext(
+              context,
+              AppLocalizations.of(context).addRandomTasksSuccess,
+            );
+          },
+        ),
+        MenuItem(
+          title: AppLocalizations.of(context).resetSession,
+          icon: Icons.delete_forever_outlined,
+          onTap: () {
+            this.clearAllTasks();
 
-                  SubtleMessage.messageWithContext(
-                    context,
-                    AppLocalizations.of(context).resetSessionSuccess,
-                  );
-                },
-              ),
-              MenuItem(
-                title: AppLocalizations.of(context).themeSwitch,
-                icon: Theme.of(context).brightness == Brightness.light
-                    ? Icons.brightness_2_outlined
-                    : Icons.wb_sunny_outlined,
-                onTap: () {
-                  KanbanSimApp.of(context).switchTheme();
-                  SubtleMessage.messageWithContext(
-                    context,
-                    AppLocalizations.of(context).themeSwitchSuccess,
-                  );
-                },
-              ),
-              MenuItem(
-                title: AppLocalizations.of(context).langSwitch,
-                icon: Icons.language_outlined,
-                children: [
-                  MenuItem(
-                    title: AppLocalizations.of(context).englishLang,
-                    onTap: () {
-                      KanbanSimApp.of(context).switchLanguageTo("english");
-                      SubtleMessage.messageWithContext(
-                        context,
-                        AppLocalizations.of(context).langSwitchSuccess,
-                      );
-                    },
-                    children: [],
-                  ),
-                  MenuItem(
-                    title: AppLocalizations.of(context).polishLang,
-                    onTap: () {
-                      KanbanSimApp.of(context).switchLanguageTo("polish");
+            SubtleMessage.messageWithContext(
+              context,
+              AppLocalizations.of(context).resetSessionSuccess,
+            );
+          },
+        ),
+        MenuItem(
+          title: AppLocalizations.of(context).themeSwitch,
+          icon: Theme.of(context).brightness == Brightness.light
+              ? Icons.brightness_2_outlined
+              : Icons.wb_sunny_outlined,
+          onTap: () {
+            KanbanSimApp.of(context).switchTheme();
+            SubtleMessage.messageWithContext(
+              context,
+              AppLocalizations.of(context).themeSwitchSuccess,
+            );
+          },
+        ),
+        MenuItem(
+          title: AppLocalizations.of(context).langSwitch,
+          icon: Icons.language_outlined,
+          children: [
+            MenuItem(
+              title: AppLocalizations.of(context).englishLang,
+              onTap: () {
+                KanbanSimApp.of(context).switchLanguageTo("english");
+                SubtleMessage.messageWithContext(
+                  context,
+                  AppLocalizations.of(context).langSwitchSuccess,
+                );
+              },
+              children: [],
+            ),
+            MenuItem(
+              title: AppLocalizations.of(context).polishLang,
+              onTap: () {
+                KanbanSimApp.of(context).switchLanguageTo("polish");
 
-                      SubtleMessage.messageWithContext(
-                        context,
-                        AppLocalizations.of(context).langSwitchSuccess,
-                      );
-                    },
-                    children: [],
-                  ),
-                ],
+                SubtleMessage.messageWithContext(
+                  context,
+                  AppLocalizations.of(context).langSwitchSuccess,
+                );
+              },
+              children: [],
+            ),
+          ],
+        ),
+        MenuItem(
+          title: AppLocalizations.of(context).info,
+          icon: Icons.info_outline_rounded,
+          onTap: () {
+            showAboutDialog(
+              context: context,
+              applicationVersion: '0.7.6',
+              applicationIcon: Icon(Icons.info_outline_rounded),
+              applicationLegalese:
+                  AppLocalizations.of(context).applicationLegalese,
+              applicationName: AppLocalizations.of(context).applicationName,
+            );
+          },
+        ),
+        MenuItem(
+          title: AppLocalizations.of(context).shareApp,
+          icon: Icons.share,
+          onTap: () {
+            Clipboard.setData(
+              ClipboardData(
+                text: "https://kamilsztandur.github.io/KanbanMethodSimApp",
               ),
-              MenuItem(
-                title: AppLocalizations.of(context).info,
-                icon: Icons.info_outline_rounded,
-                onTap: () {
-                  showAboutDialog(
-                    context: context,
-                    applicationVersion: '0.7.6',
-                    applicationIcon: Icon(Icons.info_outline_rounded),
-                    applicationLegalese:
-                        AppLocalizations.of(context).applicationLegalese,
-                    applicationName:
-                        AppLocalizations.of(context).applicationName,
-                  );
-                },
-              ),
-              MenuItem(
-                title: AppLocalizations.of(context).shareApp,
-                icon: Icons.share,
-                onTap: () {
-                  Clipboard.setData(
-                    ClipboardData(
-                      text:
-                          "https://kamilsztandur.github.io/KanbanMethodSimApp",
-                    ),
-                  );
-                  SubtleMessage.messageWithContext(
-                    context,
-                    AppLocalizations.of(context).appLinkCopiedSuccessfully,
-                  );
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
+            );
+            SubtleMessage.messageWithContext(
+              context,
+              AppLocalizations.of(context).appLinkCopiedSuccessfully,
+            );
+          },
+        ),
+      ],
     );
   }
 }
