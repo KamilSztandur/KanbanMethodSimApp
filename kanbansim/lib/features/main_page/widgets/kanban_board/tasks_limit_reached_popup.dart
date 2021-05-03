12ce88be996a9265d8fda324b3a921ba4b961380
KamilSztandur <kamil.sztandur@vp.pl>
2021-05-03 21:34:31 +0200

    Remove TasksLimitReachedPopup unused import.
    - removed l10n import from TasksLimitReachedPopup.

--- kanbansim/lib/features/main_page/widgets/kanban_board/tasks_limit_reached_popup.dart
+++ kanbansim/lib/features/main_page/widgets/kanban_board/tasks_limit_reached_popup.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:kanbansim/generated/l10n.dart';
 
 class TasksLimitReachedPopup {
   Widget show(String columnName, int limit) {
