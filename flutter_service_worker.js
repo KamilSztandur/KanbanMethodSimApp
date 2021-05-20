b4ddbca650d0ef1580a1cdd2394b581e7cc8b3a2
KamilSztandur <kamil.sztandur@vp.pl>
2021-05-20 12:01:50 +0200

    Latest release [05/20/21 12:01:50]

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "favicon.png": "83fdeef958e241bf047e1a5eef3331e0",
 "icons/Icon-192.png": "a803adfe215a81eaa5fef0a3c5393a1c",
 "icons/Icon-512.png": "ed1337c4ea280dc26ae4fbaa5e7deb21",
-"index.html": "15a4af90ad5b0ef17d42304ff71a9fb5",
-"/": "15a4af90ad5b0ef17d42304ff71a9fb5",
-"main.dart.js": "55016bd0e498b05622a2eb3d4b63d79f",
+"index.html": "aaba1e8ffa42b06ab9a5d56107ea9ff4",
+"/": "aaba1e8ffa42b06ab9a5d56107ea9ff4",
+"main.dart.js": "c89bc7435ab7eb868a50d7f6b8a87820",
 "manifest.json": "5906502537ec8fb4135157c712dd663c",
 "version.json": "af2298f529989e8ceff4133e4093118b"
 };
